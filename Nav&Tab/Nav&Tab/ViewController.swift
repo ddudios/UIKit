e11c704a2791e80a5187cded96b139c6514ea551
ddudi <ddudios@naver.com>
2024-08-04 21:25:40 +0900

    [Nav&Tab] Storyboard, Navigation Bar, Tab Bar

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by Suji Jang on 8/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
