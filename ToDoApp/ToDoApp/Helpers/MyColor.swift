26564102b2fbe5c7f0241e7dba7067749857d64d
ddudi <ddudios@naver.com>
2024-08-20 02:43:51 +0900

    [ToDoApp] UIColor+Extension

---
+++ ToDoApp/ToDoApp/Helpers/MyColor.swift
@@ -0,0 +1,69 @@
+//
+//  MyColor.swift
+//  ToDoApp
+//
+//  Created by Suji Jang on 8/20/24.
+//
+
+import UIKit
+
+// 코어데이터 저장 타입을 Int64로 설정했기 때문에
+// 타입변환을 하지 않고 편하게 사용하기 위해서 Int64로 선언
+enum MyColor: Int64 {
+    case red    = 1
+    case green  = 2
+    case blue   = 3
+    case purple = 4
+    
+    // 계산속성 -> self는 enum타입의 인스턴스 자체에 접근
+    // 연한 색상 (UIColor확장해서 16진법 사용)
+    var viewColor: UIColor {
+        switch self {
+        case .red:
+            return UIColor(hexString: "#EFD9D4")
+            //return UIColor(red: 239/255, green: 217/255, blue: 212/255, alpha: 1)
+        case .green:
+            return UIColor(hexString: "#C2EDEA")
+            //return UIColor(red: 194/255, green: 237/255, blue: 234/255, alpha: 1)
+        case .blue:
+            return UIColor(hexString: "#C4E4F2")
+            //return UIColor(red: 196/255, green: 228/255, blue: 242/255, alpha: 1)
+        case .purple:
+            return UIColor(hexString: "#D4D4F5")
+            //return UIColor(red: 212/255, green: 212/255, blue: 245/255, alpha: 1)
+        }
+    }
+    
+    // 계산속성은 실질적인 메서드니까 메서드로 구현할 수도 있다
+    // 함수를 호출하는 것보다 속성으로 접근하면 코드가 더 간편해 보인다
+    func getBackgroundColor() -> UIColor {
+        switch self {
+        case .red:
+            return UIColor(hexString: "#EFD9D4")
+            //return UIColor(red: 239/255, green: 217/255, blue: 212/255, alpha: 1)
+        case .green:
+            return UIColor(hexString: "#C2EDEA")
+            //return UIColor(red: 194/255, green: 237/255, blue: 234/255, alpha: 1)
+        case .blue:
+            return UIColor(hexString: "#C4E4F2")
+            //return UIColor(red: 196/255, green: 228/255, blue: 242/255, alpha: 1)
+        case .purple:
+            return UIColor(hexString: "#D4D4F5")
+            //return UIColor(red: 212/255, green: 212/255, blue: 245/255, alpha: 1)
+        }
+    }
+    
+    // 진한 색상
+    var buttonColor: UIColor {
+        switch self {
+        case .red:
+            return UIColor(red: 232/255, green: 163/255, blue: 153/255, alpha: 1)
+        case .green:
+            return UIColor(red: 89/255, green: 190/255, blue: 183/255, alpha: 1)
+        case .blue:
+            return UIColor(red: 88/255, green: 181/255, blue: 236/255, alpha: 1)
+        case .purple:
+            return UIColor(red: 118/255, green: 103/255, blue: 228/255, alpha: 1)
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/Helpers/UIColor+Extension.swift
@@ -0,0 +1,71 @@
+//
+//  UIColor+Extension.swift
+//  ToDoApp
+//
+//  Created by Suji Jang on 8/20/24.
+//
+
+import UIKit
+
+// UIColor는 애플이 UIKit프레임워크에 이미 만들어 놓은 타입
+// 그 타입을 직접적으로 수정은 못하고 확장은 가능 (구글링)
+extension UIColor {
+    
+    // Hex String -> UIColor
+    convenience init(hexString: String) {
+        let hexString = hexString.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hexString)
+         
+        if hexString.hasPrefix("#") {
+            scanner.currentIndex = scanner.string.index(after: scanner.currentIndex)
+        }
+         
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
+         
+        let mask = 0x000000FF
+        let r = Int(color >> 16) & mask
+        let g = Int(color >> 8) & mask
+        let b = Int(color) & mask
+         
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+         
+        self.init(red: red, green: green, blue: blue, alpha: 1)
+    }
+     
+    // UIColor -> Hex String
+    var hexString: String? {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+         
+        let multiplier = CGFloat(255.999999)
+         
+        guard self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) else {
+            return nil
+        }
+         
+        if alpha == 1.0 {
+            return String(
+                format: "#%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier)
+            )
+        }
+        else {
+            return String(
+                format: "#%02lX%02lX%02lX%02lX",
+                Int(red * multiplier),
+                Int(green * multiplier),
+                Int(blue * multiplier),
+                Int(alpha * multiplier)
+            )
+        }
+    }
+}
+
+// 출처: https://www.hangge.com/blog/cache/detail_2305.html
